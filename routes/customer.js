be7b4e0b0b7d80114c88f50ef3aca2fc5e7419f8
FELIPE SANTOS AYALA TUME <fsayalat@hotmail.com>
2019-06-21 20:10:56 +0000

    cambio a otro perfil

--- routes/customer.js
+++ routes/customer.js
@@ -23,7 +23,7 @@ function setGetCustomerOptions(req, res) {
     protocol: "http",
     host: _apis.customer.host,
     api: _apis.customer.base_path,
-    operation: "customer/search?username=foo"
+    operation: "customer/search?username=Felipe"
   });
 
   var options = {
